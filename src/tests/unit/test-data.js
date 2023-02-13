9f4d86f40390e8e4cb6a629577f2d81d37e29aba
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2023-02-13 10:05:59 -0800

    Fix unit test

--- src/tests/unit/test-data.js
+++ src/tests/unit/test-data.js
@@ -8,6 +8,9 @@ const fakeIdentitiesTestCases = [
       onid: 'potterharry2000',
       osuUid: '1234567890',
       proxId: '1234-1234567',
+      displayFirstName: 'Harry',
+      displayMiddleName: 'James',
+      displayLastName: 'Potter',
     },
     {
       firstName: 'Tom',
@@ -17,6 +20,9 @@ const fakeIdentitiesTestCases = [
       onid: 'voldemort',
       osuUid: '0123456789',
       proxId: '5678-7654321',
+      displayFirstName: 'Tom',
+      displayMiddleName: 'Marvolo',
+      displayLastName: 'Riddle',
     },
   ],
   [
@@ -28,6 +34,9 @@ const fakeIdentitiesTestCases = [
       onid: 'antihero',
       osuUid: '9999999999',
       proxId: '1111-2222222',
+      displayFirstName: 'Severus',
+      displayMiddleName: null,
+      displayLastName: 'Snape',
     },
   ],
 ];
@@ -41,6 +50,9 @@ const fakeIdentityTestCases = [
     onid: 'wizard',
     osuUid: '1111111111',
     proxId: '0000-3333333',
+    displayFirstName: 'Albus',
+    displayMiddleName: null,
+    displayLastName: 'Dumbledore',
   },
 ];
 
